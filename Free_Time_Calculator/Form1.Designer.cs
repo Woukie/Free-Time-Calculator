36cadc36d2bc7cffd5f399d98173977560cae706
Lucas Eldon <60012718+Woukie@users.noreply.github.com>
2021-02-13 19:34:31 +0000

    Initial Commit

---
+++ Free_Time_Calculator/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Free_Time_Calculator
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.btn_start = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.nudMarks = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMinutesWorked = new System.Windows.Forms.Label();
+            this.lblWorkPlay = new System.Windows.Forms.Label();
+            this.nudPlay = new System.Windows.Forms.NumericUpDown();
+            this.nudWork = new System.Windows.Forms.NumericUpDown();
+            this.lblMarks = new System.Windows.Forms.Label();
+            this.lblFreeTime = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTotalWorked = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.lblTotalEarned = new System.Windows.Forms.Label();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPausePlay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMarks)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWork)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            resources.ApplyResources(this.btn_start, "btn_start");
+            this.btn_start.Name = "btn_start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // nudMarks
+            // 
+            resources.ApplyResources(this.nudMarks, "nudMarks");
+            this.nudMarks.Name = "nudMarks";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.lblMinutesWorked);
+            this.panel1.Controls.Add(this.lblWorkPlay);
+            this.panel1.Controls.Add(this.nudPlay);
+            this.panel1.Controls.Add(this.nudWork);
+            this.panel1.Controls.Add(this.lblMarks);
+            this.panel1.Controls.Add(this.nudMarks);
+            this.panel1.Controls.Add(this.lblFreeTime);
+            this.panel1.Name = "panel1";
+            // 
+            // lblMinutesWorked
+            // 
+            resources.ApplyResources(this.lblMinutesWorked, "lblMinutesWorked");
+            this.lblMinutesWorked.Name = "lblMinutesWorked";
+            // 
+            // lblWorkPlay
+            // 
+            resources.ApplyResources(this.lblWorkPlay, "lblWorkPlay");
+            this.lblWorkPlay.Name = "lblWorkPlay";
+            // 
+            // nudPlay
+            // 
+            resources.ApplyResources(this.nudPlay, "nudPlay");
+            this.nudPlay.Name = "nudPlay";
+            this.nudPlay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudWork
+            // 
+            resources.ApplyResources(this.nudWork, "nudWork");
+            this.nudWork.Name = "nudWork";
+            this.nudWork.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lblMarks
+            // 
+            resources.ApplyResources(this.lblMarks, "lblMarks");
+            this.lblMarks.Name = "lblMarks";
+            // 
+            // lblFreeTime
+            // 
+            resources.ApplyResources(this.lblFreeTime, "lblFreeTime");
+            this.lblFreeTime.Name = "lblFreeTime";
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Controls.Add(this.lblTotalWorked);
+            this.panel2.Controls.Add(this.txtLog);
+            this.panel2.Controls.Add(this.lblTotalEarned);
+            this.panel2.Controls.Add(this.lblLog);
+            this.panel2.Name = "panel2";
+            // 
+            // lblTotalWorked
+            // 
+            resources.ApplyResources(this.lblTotalWorked, "lblTotalWorked");
+            this.lblTotalWorked.Name = "lblTotalWorked";
+            // 
+            // txtLog
+            // 
+            resources.ApplyResources(this.txtLog, "txtLog");
+            this.txtLog.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            // 
+            // lblTotalEarned
+            // 
+            resources.ApplyResources(this.lblTotalEarned, "lblTotalEarned");
+            this.lblTotalEarned.Name = "lblTotalEarned";
+            // 
+            // lblLog
+            // 
+            resources.ApplyResources(this.lblLog, "lblLog");
+            this.lblLog.Name = "lblLog";
+            // 
+            // btnClear
+            // 
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPausePlay
+            // 
+            resources.ApplyResources(this.btnPausePlay, "btnPausePlay");
+            this.btnPausePlay.Name = "btnPausePlay";
+            this.btnPausePlay.UseVisualStyleBackColor = true;
+            this.btnPausePlay.Click += new System.EventHandler(this.btnPausePlay_Click);
+            // 
+            // Form
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnPausePlay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMarks)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWork)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NumericUpDown nudMarks;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblMarks;
+        private System.Windows.Forms.Label lblFreeTime;
+        private System.Windows.Forms.Label lblWorkPlay;
+        private System.Windows.Forms.NumericUpDown nudPlay;
+        private System.Windows.Forms.NumericUpDown nudWork;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTotalEarned;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Button btnPausePlay;
+        private System.Windows.Forms.Label lblMinutesWorked;
+        private System.Windows.Forms.Label lblTotalWorked;
+    }
+}
+
---
+++ Free_Time_Calculator/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Free_Time_Calculator
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private int _marks;
+        private float _freeTimeEarned, _workToPlayRatio, _totalFreeTimeEarned, _totalTimeWorked;
+        private long _timeStarted, _timeWhenPaused;
+        private bool _running, _paused;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            _running = !_running;
+
+            if (_running)
+            {
+                btn_start.Text = "Submit";
+
+                btnPausePlay.Enabled = true;
+                nudMarks.Enabled = false;
+                nudWork.Enabled = false;
+                nudPlay.Enabled = false;
+
+                _marks = int.Parse(nudMarks.Text);
+                _workToPlayRatio = float.Parse(nudWork.Text) / float.Parse(nudPlay.Text);
+                _timeStarted = DateTime.Now.Ticks;
+
+                timer.Start();
+
+                return;
+            }
+
+            float sessionTimeWorked = (DateTime.Now.Ticks - _timeStarted) / 600000000f;
+            _totalFreeTimeEarned += _freeTimeEarned;
+            _totalTimeWorked += sessionTimeWorked;
+
+            txtLog.Text += "Earned " + _freeTimeEarned + " in " + sessionTimeWorked + " minutes" + "\n";
+            lblTotalEarned.Text = "Total time earned: " + _totalFreeTimeEarned;
+            lblTotalWorked.Text = "Total time worked: " + _totalTimeWorked;
+            btn_start.Text = "Start";
+
+            btnPausePlay.Enabled = false;
+            nudMarks.Enabled = true;
+            nudWork.Enabled = true;
+            nudPlay.Enabled = true;
+
+            timer.Stop();
+        }
+
+        private void btnPausePlay_Click(object sender, EventArgs e)
+        {
+            if (_paused)
+            {
+                _paused = false;
+                _timeStarted += DateTime.Now.Ticks - _timeWhenPaused;
+                btnPausePlay.Text = "Pause";
+
+                btn_start.Enabled = true;
+
+                timer.Start();
+
+                return;
+            }
+
+            _paused = true;
+            _timeWhenPaused = DateTime.Now.Ticks;
+            btnPausePlay.Text = "Play";
+
+            btn_start.Enabled = false;
+
+            timer.Stop();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _totalTimeWorked = 0;
+            _totalFreeTimeEarned = 0;
+            lblTotalEarned.Text = "Total time earned: ";
+            lblTotalWorked.Text = "Total time worked: ";
+            txtLog.Text = "";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (_timeStarted == 0) return;
+            if (!_running) timer.Stop();
+
+            _freeTimeEarned = 2 * _marks * _marks / (_workToPlayRatio * (_marks + (DateTime.Now.Ticks - _timeStarted) / 600000000f));
+
+            lblMinutesWorked.Text = "Minutes worked: " + (DateTime.Now.Ticks - _timeStarted) / 600000000f;
+            lblFreeTime.Text = "Minutes of free time earned: " + _freeTimeEarned;
+        }
+    }
+}
